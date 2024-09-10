b80b32dfcc75088b6fb69b53431999ebecef37c5
Ahmed Samir <ahmed123.as27@gmail.com>
2024-09-10 09:16:23 +0400

    First trial of fine-tuned model

--- finetuned_model_testing/testing_quantized_fine_tuned_model.py
+++ finetuned_model_testing/testing_quantized_fine_tuned_model.py
@@ -2,4 +2,4 @@ from gpt4all import GPT4All
 
 model = GPT4All("C:\\llama-3-8b-full-chat-aou-Q4_K_M\\llama-3-8b-full-chat-aou-Q4_K_M.gguf", device='gpu')  # device='amd', device='intel'
 with model.chat_session():
-    print(model.generate("اخبرني عن دكتور يوسف", max_tokens=100))
+    print(model.generate("كم يوسف تعرف؟", max_tokens=100))
